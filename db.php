2b223a7f31cf322000da37eac88e2046829a693c
Dhriti Saha <39408900+dhritisaha13@users.noreply.github.com>
2020-08-19 10:23:01 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php 
+
+$dbhost = 'localhost';
+$dbname = 'chat application';
+$dbuser = 'root';
+$dbpass = '';
+
+$db = new PDO("mysql:dbhost=$dbhost;dbname=$dbname", "$dbuser", "$dbpass");
+
+
+
+ ?>
